a813025d7e4d675c81a641e372b588ef2ad07c88
harishraghav03 <harishraghav@yectra.com>
2024-08-05 21:31:33 +0530

    Updated

---
+++ Fundamentals/dist/index.js
@@ -0,0 +1,21 @@
+"use strict";
+let sales = 123;
+let course = 'TypeScript';
+let isPublished = true;
+let level;
+function render(document) {
+    console.log(document);
+}
+let numbers = [1, 2, 3];
+numbers.forEach(n => n.toString());
+let user = [1, 'Raghav'];
+user[0].valueOf();
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+;
+let mySize = Size.Large;
+console.log(mySize);
--- Fundamentals/src/index.ts
+++ Fundamentals/src/index.ts
@@ -8,4 +8,61 @@ let level; // It assumes to be any -> Which kinda represents any kind of value
 
 function render(document : any){  // Now we have a compilation error -> we can annotate
     console.log(document);
+} // What if we have tons of error like this, don't do that -> NoImplicitAny (false)
+
+// Arrays -> Each element can have diff type
+
+// let number = [1, 2, 3];
+// What if a func only accepts number, we can annotate
+let numbers: number[] = [1, 2, 3];
+// If the array is empty, it is any 
+numbers.forEach(n => n.toString()); // We cannot find in JS
+
+// Tuples
+
+// 1, 'Harish'
+
+let user: [number, string] = [1, 'Raghav'];
+user[0].valueOf(); //  We can have no of methods related to its type
+// Internally they are represented in plain JS arrays
+// They are usefull when there are short, like key:value pairs
+// Tuple is fixed length array
+
+// Enums
+
+// List of related constants
+
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+// PascalName
+
+enum Size { Small = 1, Medium, Large };  // By default it assigns to 0, 1, 2 ...
+// If we use string in first, we have to explicity set the value for each member
+// Tip: set enum as const, it will give optimized JS code
+
+let mySize: Size = Size.Large;
+console.log(mySize);
+
+// Functions
+
+// function calculateTax(income: number) : number{ // If we set the body as blank, it tells it is void
+//     return 0; // Now the type of the return value is number 
+// } // The compiler inferred the type, which is great
+// As a best practice, we should annotate (parameter and return type)
+// NoUnusedParameters -> It will warn
+
+function calculateTax (income :number, taxNumber = 2022) : number {
+    let x;
+    if (taxNumber < 50_000)
+        return income * 0.25; // Now we have compilation error, if the above if is false, by default it returns undefined
+    // If we remove the return type annotation, the error will be gone
+    // NoImplicitReturns -> It will warn
+    return 0.25;
+    // Detecting unused variables -> NoUnusedLocals
 }
+// If we want to call this function, we should pass the only 2 args
+// If you want to pass only 1 arg, use ?, but if we don't pass the args by default it is undefined Compilation error
+// To tackle this we use or operator, but there will be different appraoch
+// We give the default value in the parameter itself
