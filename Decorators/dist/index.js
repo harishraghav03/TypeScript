f60be02e61ee22d05ff4fed75e1184fd22aa8f51
harishraghav03 <harishraghav@yectra.com>
2024-08-12 15:40:42 +0530

    Updated

--- Decorators/dist/index.js
+++ Decorators/dist/index.js
@@ -1,3 +1,4 @@
+"use strict";
 /* Decorators
 They are attributes that we apply to the classes and their members with this we can change
 how they behave
@@ -6,5 +7,5 @@ It is just a function called by JS runtime
 The JS compiler calls the function and passes the class to it, in that function have can modify our class
 new properites, new methods or we can change the implementation of the existing methods
 Before to create any decorator, we have to enable a special compiler option, they are experimental features
-their standards and implementation features might change in the future  
-*/
+their standards and implementation features might change in the future
+*/ 
