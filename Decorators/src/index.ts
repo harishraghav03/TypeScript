16434fe752dfcc6910bc4483fd744bcdbf0f941a
harishraghav03 <harishraghav@yectra.com>
2024-08-12 16:46:39 +0530

    Updated

--- Decorators/src/index.ts
+++ Decorators/src/index.ts
@@ -8,3 +8,23 @@ new properites, new methods or we can change the implementation of the existing 
 Before to create any decorator, we have to enable a special compiler option, they are experimental features
 their standards and implementation features might change in the future  
 */
+
+// Class Decorators
+
+// Depending on where we gonna apply this decorator the type of parameters will varies
+// If we gonna apply to this class, here should have single parameter that represents our constructor function
+// What matters is the type of the parameter, if it is function, the runtime assumes that we gonna applies this to the class
+
+// function Component(constructor: Function) {
+//     // No limitation
+//     console.log('Component Decorator called');
+//     constructor.prototype.uniqueId = Date.now();
+//     constructor.prototype.insertInDOM = () => {
+//         console.log('Inserting Component in the DOM');
+//     }
+// } We can also solve this using inheritance
+
+// @Component
+// class ProfileComponent {
+// }
+
