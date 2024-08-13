9d39e5f6134bdbff527a677d7880a1f7e747e28f
harishraghav03 <harishraghav@yectra.com>
2024-08-13 14:46:14 +0530

    Modules

--- Modules/dist/index.js
+++ Modules/dist/index.js
@@ -1,1 +1,6 @@
 "use strict";
+/* We should split our files into modules */
+Object.defineProperty(exports, "__esModule", { value: true });
+const shapes_1 = require("./shapes");
+let circle = new shapes_1.Circle(1);
+console.log(circle.radius);
---
+++ Modules/src/index.ts
@@ -0,0 +1,8 @@
+/* We should split our files into modules */
+
+import {Circle as MyCircle} from './shapes';
+
+let circle = new MyCircle(1);
+console.log(circle.radius);
+
+
